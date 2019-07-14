0e7ff16dc6615839e01e500f9742849f659fe701
deion <db101097@gmail.com>
2019-07-14 19:20:01 -0400

    textfield input can be grabbed

--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -2,24 +2,69 @@ import React, { Component } from 'react';
 
 
 class Register extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            firstName:"",
+            lastName:"",
+            email:"",
+            password:"",
+            confirmPassword:"",
+            valid:false
+        }
+
+        this.handleInput= this.handleInput.bind(this)
+        this.validate=this.validate.bind(this)
+    }
+
+    validate = ()=> {
+        
+    }
+
+    handleInput= (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    submit = (e)=>{
+        e.preventDefault();
+        console.log("firstname",this.state.firstName)
+        console.log("lastname",this.state.lastName)
+        console.log("email",this.state.email)
+        console.log("password",this.state.password)
+        console.log("confirm password",this.state.confirmPassword)
+    }
+
+
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form>
                 <label>
-                 Name:
+                 First Name:
                 <br/>
-                <input type="text" />
+                <input type="text" name="firstName" onChange={this.handleInput}/>
+                <br/>
+                 Last Name:
+                <br/>
+                <input type="text" name="lastName" onChange={this.handleInput}/>
                 <br/>
                 Email:
                 <br/>
-                <input type="text" />
+                <input type="text" name="email" onChange={this.handleInput}/>
                 <br/>
                 Password:
                 <br/>
-                <input type="password" />
+                <input type="password" name="password" onChange={this.handleInput}/>
+                <br/>
+                Confirm Password:
+                <br/>
+                <input type="password" name="confirmPassword" onChange={this.handleInput}/>
                 </label>
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={this.submit}/>
             </form>
         )
     }

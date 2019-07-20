5c5e35d872e19d676b33a3d26ae0e3b30695d458
deion <db101097@gmail.com>
2019-07-20 01:45:22 -0400

    set up function prototypes for the register route and created the user schema

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,40 @@
-module.exports=function(user){
-    
+const bcrypt = require('bcryptjs')
+const jwt=require('jsonwebtoken')
+
+/*
+    The function takes in a password as
+    a param. The password is then hashed 
+    using bcrypt and then returned the hashed
+    password
+*/
+function createHash(pw){
+
+}
+
+/*
+    The function takes in an email as a param.
+    It will query the database using the email 
+    to assure that the user does not already exist 
+    within the database. It will return true if the user 
+    exists and false if the user does not exist.
+*/
+function userExist(email){
+
+}
+
+/*
+    The function takes in a form object then
+    it will use the validatejs library to validate
+    the data to make sure no bad data is able to make
+    it to the database.
+*/
+function validate(form){
+
+}
+
+module.exports=function(user,app){
+
+    app.get("/register",async function (req,res){
+          
+    })
 }

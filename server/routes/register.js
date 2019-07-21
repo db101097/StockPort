4411abf0ff92f2422ff5c4f49a83beacbfd5022d
deion <db101097@gmail.com>
2019-07-20 23:17:49 -0400

    Backend validation completed

--- server/routes/register.js
+++ server/routes/register.js
@@ -11,28 +11,50 @@ function createHash(pw){
 
 }
 
-/*
-    The function takes in an email as a param.
-    It will query the database using the email 
-    to assure that the user does not already exist 
-    within the database. It will return true if the user 
-    exists and false if the user does not exist.
-*/
-function userExist(email){
+function insertUser(newUser){
+    newUser.save()
+        .then(()=>{
+            return "Success, your account has been created"
+        })
+        .catch((err)=>{
+            return "This account could not be created or already exists"
+        })
+}
 
+async function validate(newUser){
+    try{
+        let err = await newUser.validate()
+        console.log('error ',err)
+        return true
+    }
+    catch(err){
+        console.log(err)
+        return false
+    }
 }
 
 /*
-    The function takes in a fuser object then
-    it will use the validatejs library to validate
-    the data to make sure no bad data is able to make
-    it to the database.
+    The function takes in a user object and the
+    user model then it will run validation on it.
+    If the data is valid it is inserted into the 
+    database
 */
-function validate(user){
+async function createUser(user,userModel){
+        //create a userModel object with the values
+        let newUser=await userModel.build(user)
+        //validate the values of the new user
+        let valid = await validate(newUser)
 
+        if(valid===true){
+            let result=insertUser(newUser,userModel)
+            res.status(200).send(result)
+        }
+        else{
+            res.status(400).send("The account could not be created")
+        }
 }
 
-module.exports=function(app,user){
+module.exports=function(app,userModel){
 
     //register a user route
     app.post("/register",async function (req,res){
@@ -42,6 +64,8 @@ module.exports=function(app,user){
             email:req.body.email,
             password:req.body.password
         }
+
+        createUser(user,userModel)
 
         res.status(200).send({msg:user})
     })

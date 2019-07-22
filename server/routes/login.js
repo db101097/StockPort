96c5b89d5030d9897d54acd8783bd998c7aad399
deion <db101097@gmail.com>
2019-07-21 21:43:29 -0400

    Added route for login feature

---
+++ server/routes/login.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt')
+
+
+/*
+
+*/
+async function issueToken(password){
+}
+
+/*
+
+*/
+async function decrypt(newUser){
+
+}
+
+/*
+
+*/
+async function authenticate(user,userModel){
+  
+}
+
+/*
+
+*/
+async function findUser(user,userModel){
+
+}
+
+module.exports=function(app,userModel){
+
+    //register a user route
+    app.post("/login",async function (req,res){
+        
+    })
+}

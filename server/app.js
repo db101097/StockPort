728fe96bcea3454747b89c73a6af591a19d95ec7
deion <db101097@gmail.com>
2019-09-13 17:44:01 -0400

    adding cors handling for new hosted address

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const jwtCheck = expressjwt({secret:process.env.SECRET_KEY})
 
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Origin', "https://stockport-ttp.herokuapp.com");
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept,authorization'
